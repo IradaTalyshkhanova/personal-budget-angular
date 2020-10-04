0da52de5983798986bca22dfac703c325d9fe040
italyshk <italyshk@uncc.edu>
2020-10-04 15:35:36 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,25 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 3000; 
+const budget = require('./server_s3.json');
 
 app.use('/', express.static('public'));
-
-const budget = { 
- myBudget:[
-    {
-        title:'Eat out',
-        budget: 25
-    },
-    {
-        title:'Rent',
-        budget: 375
-    },
-    {
-        title:'Grocery',
-        budget: 110
-    },
-  ]
-}; 
 
 app.get('/hello', (reg, res)=> {
     res.send('Hello World!');
